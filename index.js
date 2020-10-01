24ea5ab8cc996dd7c55ea4d9e02cc15d8f87edc9
touhid1 <touhiduliislam5@gmail.com>
2020-10-02 02:18:21 +0600

    Allhamdullilah

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config()
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nmory.azure.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = 5000
+
+const client = new MongoClient(uri, { useNewUrlParser: true ,useUnifiedTopology: true });
+client.connect(err => {
+  const productsCollection = client.db("emaJohn").collection("myAdmin");
+  const ordersCollection = client.db("emaJohn").collection("orders");
+
+app.post("/addProduct",(req,res) => {
+const products =req.body;
+productsCollection.insertOne(products)
+.then(result =>{
+console.log(result.insertedCount);
+res.send(result.insertedCount)
+})
+})
+
+app.get('/products',(req,res) =>{
+  productsCollection.find({})
+  .toArray((err,documents) => {
+    res.send(documents)
+  })
+})
+
+app.get('/product/:kay',(req,res) =>{
+  productsCollection.find({key:req.params.kay})
+  .toArray((err,documents) => {
+    res.send(documents[0])
+  })
+})
+
+app.post('/productsByKeys',(req,res) =>{
+  const productKeys = req.body;
+  productsCollection.find({key: {$in: productKeys}})
+  .toArray((err,documents) =>{
+    res.send(documents)
+  })
+})
+app.post("/addOrder",(req,res) => {
+  const order =req.body;
+  ordersCollection.insertOne(order)
+  .then(result =>{
+  res.send(result.insertedCount > 0)
+  })
+  
+  })
+});
+
+
+
+app.listen(port)
